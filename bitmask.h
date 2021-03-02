3f74e9c38a6920d3f1a109076dc55d40e07a1438
99003696 <78849844+99003696@users.noreply.github.com>
2021-03-02 14:21:23 +0530

    Delete bitmask.h

--- bitmask.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BITMASK_H
-#define __BITMASK_H
-#define SET 1
-#define NOTSET 0
-int set(int n, int k);
-int clear(int n, int k);
-int flip(int n, int k);
-int query(int n, int k);
-#endif
